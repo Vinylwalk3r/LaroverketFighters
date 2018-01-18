4b649c722d89f710f2fe127585322555a1f69550
Johannes Wikström <35334790+Vinylwalk3r@users.noreply.github.com>
2018-01-18 14:38:19 +0100

    idk

--- Program/Program.cs
+++ Program/Program.cs
@@ -10,7 +10,6 @@ namespace Program
     {
         static Random randomness = new Random();
         static int playerHP = randomness.Next(10, 200);
-        static int enemyHP = randomness.Next(10, 200);
 
 
         static void Main(string[] args)
@@ -18,6 +17,8 @@ namespace Program
             Console.Title = "TextFeud";
             Console.SetWindowSize(34,14);
 
+            enemy.Setup();
+            
 
             string userInput;
 
@@ -55,7 +56,7 @@ namespace Program
                 Console.ReadKey();
                 //End of While-loop
             }
-            if (enemyHP > 1)
+            if (enemy.hp > 1)
             {
                 Console.WriteLine("Sorry, your dead...");
             }
@@ -73,13 +74,13 @@ namespace Program
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nPlayer Health: " + playerHP);
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Enemy Health: " + enemyHP);
+            Console.WriteLine("Enemy Health: " + enem.hp);
         }
 
         static void Attack()
         {
             int playerDmg = randomness.Next(10, 16);
-            enemyHP -= playerDmg; //subtracts player damage from enemyhp
+            enemy.hp -= playerDmg; //subtracts player damage from enemyhp
             Console.WriteLine("Player attacked for " + playerDmg);
         }
 
@@ -95,14 +96,13 @@ namespace Program
             if(randomness.Next(10-1) >= 3)
             {
                 //Enemy Attacks
-                int enemyDmg = randomness.Next(12, 18);
-                playerHP -= enemyDmg;
+                playerHP -= enemy.dmg;
                 Console.WriteLine("Enemy attacked for " + enemyDmg);
             }
             else
             {
                 int healAmount = randomness.Next(12, 15);
-                enemyHP += healAmount;
+                enemy.hp += healAmount;
                 Console.WriteLine("Enemy healed for " + healAmount);
             }
 
---
+++ Program/enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    public class Enemy
+    {
+        public int hp, dmg;
+        public string name;
+
+        public void Setup()
+        {
+            Random randomness = new Random();
+
+            hp = randomness.Next(10, 200);
+
+            dmg = randomness.Next(12, 18);
+
+            name = "Kenzykatz";
+        }
+    }
+}
